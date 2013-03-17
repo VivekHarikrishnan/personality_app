bc03b937995472dcf86ecc9f6b24f1efa01fd44a
Vivek Harikrishnan <r.vivekharikrishnan@gmail.com>
2013-03-17 07:50:00 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PersonalityApp::Application.config.secret_token = '8babf5e8779b04abd8bebd5cf63e06fcc81734933cd85ac540961b30767c167beeaa90a33a7a0d849acd8db3fdaea379605af90ab302c94acf78af8a6025f4ad'
